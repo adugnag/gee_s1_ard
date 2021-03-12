bda2151e16545641000dfd97212d208b6babee8d
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2021-03-12 10:43:57 +0100

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  6 10:55:16 2021
-
-@author: adugnamullissa
+Version: v1.0
+Date: 2021-03-12
+Authors: Mullissa A., Vollrath A.,  Reiche J., Slagter B., Balling J. , Gou Y., Braun, C.
 """
 
 import ee
 import border_noise_correction as bnc
 import speckle_filter as sf
 import terrain_flattening as trf
+import helper
 
 ee.Initialize()
 
-#---------------------------------------------------------------------------//
-# Linear to db scale
-#---------------------------------------------------------------------------//
-
-def lin_to_db(image):
-  """ Convert backscatter from linear to dB. """
-  bandNames = image.bandNames().remove('angle')
-  db = ee.Image.constant(10).multiply(image.log10()).rename(bandNames)
-  return image.addBands(db, None, True)
-
-def lin_to_db2(image):
-    """ Convert backscatter from linear to dB by removing the ratio band. """
-    if (image.bandNames().contains('VH')):
-        db = ee.Image(10).multiply(image.select(['VV', 'VH']).log10()).rename(['VV', 'VH'])
-    return image.addBands(db)
-
-def add_ratio_lin(image):
-    """ Adding ratio band for visualization """
-    ratio = image.addBands(image.select('VV').divide(image.select('VH')).rename('VVVH_ratio'))
-    return ratio.set('system:time_start', image.get('system:time_start'))
-
 
 ###########################################
-# 1. DO THE JOB
+# DO THE JOB
 ###########################################
 
 def s1_preproc(params):
@@ -179,7 +159,7 @@ def s1_preproc(params):
     ####################### 
     
     if (FORMAT == 'DB'):
-        s1_1 = s1_1.map(lin_to_db)
+        s1_1 = s1_1.map(helper.lin_to_db)
         
     ########################
     # 8. EXPORT ASSET
