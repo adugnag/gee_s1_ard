a824b31c4225bf2afaca5d9853542e20b27e450d
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2021-04-03 19:35:54 +0200

    Update helper.py

--- python-api/helper.py
+++ python-api/helper.py
@@ -8,26 +8,67 @@ Authors: Mullissa A., Vollrath A.,  Reiche J., Slagter B., Balling J. , Gou Y., 
 
 import ee
 
-#---------------------------------------------------------------------------//
+# ---------------------------------------------------------------------------//
 # Linear to db scale
-#---------------------------------------------------------------------------//
+# ---------------------------------------------------------------------------//
 
 def lin_to_db(image):
-    """ Convert backscatter from linear to dB. """
+    """
+    Convert backscatter from linear to dB.
+
+    Parameters
+    ----------
+    image : ee.Image
+        Image to convert 
+
+    Returns
+    -------
+    ee.Image
+        output image
+
+    """
     bandNames = image.bandNames().remove('angle')
     db = ee.Image.constant(10).multiply(image.log10()).rename(bandNames)
     return image.addBands(db, None, True)
 
 def lin_to_db2(image):
-    """ Convert backscatter from linear to dB by removing the ratio band. """
+    """
+    Convert backscatter from linear to dB by removing the ratio band.
+
+    Parameters
+    ----------
+    image : ee.Image
+        Image to convert
+
+    Returns
+    -------
+    ee.Image
+        Converted image
+
+    """
+
     db = ee.Image.constant(10).multiply(image.select(['VV', 'VH']).log10()).rename(['VV', 'VH'])
     return image.addBands(db)
 
-#---------------------------------------------------------------------------//
+# ---------------------------------------------------------------------------//
 # Add ratio bands
-#---------------------------------------------------------------------------//
+# ---------------------------------------------------------------------------//
 
 def add_ratio_lin(image):
-    """ Adding ratio band for visualization """
+    """
+    Adding ratio band for visualization 
+
+    Parameters
+    ----------
+    image : ee.Image
+        Image to use for creating band ratio
+
+    Returns
+    -------
+    ee.Image
+        Image containing the ratio band
+
+    """
+
     ratio = image.addBands(image.select('VV').divide(image.select('VH')).rename('VVVH_ratio'))
     return ratio.set('system:time_start', image.get('system:time_start'))
