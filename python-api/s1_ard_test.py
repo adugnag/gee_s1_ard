733ce2699d5c6122cfe59802bd300dbfd95f63a8
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2021-04-23 13:05:25 +0200

    Update s1_ard_test.py

--- python-api/s1_ard_test.py
+++ python-api/s1_ard_test.py
@@ -6,9 +6,9 @@ Date: 2021-03-12
 Authors: Mullissa A., Vollrath A.,  Reiche J., Slagter B., Balling J. , Gou Y., Braun, C.
 Description: This script creates an analysis ready S1 image collection.
 
-    params:
+    Params:
         geometry: The region to include imagery within. (ee.Geometry); The user has to interactively draw a bounding box within the map window.
-        ORBITS:  The orbits to include. (string: ASCENDING, DESCENDING OR BOTH)
+        ORBIT:  The orbits to include. (string:BOTH, ASCENDING or DESCENDING)
         START_DATE: The earliest date to include images for (inclusive).
         END_DATE: The latest date to include images for (exclusive).
         POLARIZATION: The Sentinel-1 image polarization to select for processing.
@@ -16,8 +16,7 @@ Description: This script creates an analysis ready S1 image collection.
             'VH' - selects the VH polarization.
             "VVVH' - selects both the VV and VH polarization for processing.
         APPLY_SPECKLE_FILTERING: true or false options to apply speckle filter
-        SPECKLE_FILTER: (Optional)   Type of speckle filtering to apply. Can be monotemporal or multitemporal.
-            This is done by passing the argument 'MONO' or 'MULTI'.
+        SPECKLE_FILTER: (Optional)   Type of speckle filtering to apply. 
             'BOXCAR' - implements a boxcar filter on each individual image in the collection
             'LEE' - implements a Lee filter on each individual image in the collection based on (J.S. Lee et al. 1980)
             'GAMMA MAP' - implements a Gamma maximum a-posterior speckle filter on each individual image in the collection based on (Lopez et al. 1990
@@ -29,19 +28,16 @@ Description: This script creates an analysis ready S1 image collection.
                                   the filtering is applied to each image in the collection individually. Whereas, the 
                                   Multitemporal Speckle filter based on the approach by Quegan and Yu 2001 with any of the above mentioned speckle filters.
         SPECKLE_FILTER_KERNEL_SIZE: is the size of the filter spatial window applied in speckle filtering. must be a positive integer.
-        NR_OF_IMAGES: is the number of multi-temporal images to use in the multi-temporal filter framework.
+        NR_OF_IMAGES: is the number of images to use in the multi-temporal filter framework.
         APPLY_BORDER_NOISE_CORRECTION: (Optional) true or false options to apply additional Border noise correction:
-        TERRAIN_FLATTENING - (Optional) true or false option to apply Terrain correction based on Vollrath et al. 2020 and Hoekman and Reiche 2015 is applied. 
-        TERRAIN_FLATTENING_MODEL - which model to use for radiometric terrain flattening (DIRECT, or VOLUME)
-        DEM - which digital elevation model (DEM) to use (as EE asset)
-        TERRAIN_FLATTENING_ADDITIONAL_LAYOVER_SHADOW_BUFFER - buffer parameter for layover/shadow mask in meters
-        FORMAT - the output format for the processed collection. this can be 'LINEAR' or 'DB'.
-        CLIP_TO_ROI - (Optional) Resizes the processed images to the region of interest.
-        SAVE_ASSETS - (Optional) Exports the processed collection to an asset.
-
+        TERRAIN_FLATTENING : (Optional) true or false option to apply Terrain correction based on Vollrath et al. 2020 and Hoekman and Reiche 2015 is applied. 
+        TERRAIN_FLATTENING_MODEL : which model to use for radiometric terrain flattening (DIRECT, or VOLUME)
+        DEM : which digital elevation model (DEM) to use (as EE asset)
+        TERRAIN_FLATTENING_ADDITIONAL_LAYOVER_SHADOW_BUFFER : buffer parameter for layover/shadow mask in meters
+        FORMAT : the output format for the processed collection. this can be 'LINEAR' or 'DB'.
+        SAVE_ASSETS : (Optional) Exports the processed collection to an asset.
     Returns:
-        (s1_processed) where
-            s1_processed (ee.ImageCollection): preprocessed image collection
+        An ee.ImageCollection with an analysis ready Sentinel 1 imagery with the specified polarization images and angle band.
 
     """
 
