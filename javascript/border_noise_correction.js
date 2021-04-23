63d53d26d5c19b15a0e50f73594bae7c67e7b699
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2021-04-23 10:57:37 +0200

    Update border_noise_correction.js

--- javascript/border_noise_correction.js
+++ javascript/border_noise_correction.js
@@ -1,38 +1,40 @@
-/*
-Version: v1.0
-Date: 2021-03-12
-Authors: Mullissa A., Vollrath A.,  Reiche J., Slagter B., Balling J. , Gou Y., Braun, C.
-Description: Function to mask out edges of images using using angle.
-    Adopted from: Hird et al. 2017 Remote Sensing (supplementary material): http://www.mdpi.com/2072-4292/9/12/1315)
-*/
+/* File: border_noise_correction.js
+Version: v1.1
+Date: 2021-03-11
+Authors: Adopted from Hird et al. 2017 Remote Sensing (supplementary material): http://www.mdpi.com/2072-4292/9/12/1315)
+Description: This script applied additional border noise correction */
+
+var helper = require('users/nrtwur/S1_ARD_TEST:utilities');
+
 //---------------------------------------------------------------------------//
 // Additional Border Noise Removal
 //---------------------------------------------------------------------------//
- /* (mask out angles >= 45.23993) */
- var maskAngLT452 = function(image) {
-    var ang = image.select(['angle']);
-    return image.updateMask(ang.lt(45.23993)).set('system:time_start', image.get('system:time_start'));
-   };
-   
-   /** Function to mask out edges of images using angle.
-    * (mask out angles <= 30.63993) */
-   var maskAngGT30 = function(image) {
-    var ang = image.select(['angle']);
-    return image.updateMask(ang.gt(30.63993)).set('system:time_start', image.get('system:time_start'));
-   };
-   
-   /** Remove edges.
-    * Source: Andreas Vollrath */
-   var maskEdge = function(image) {
-     var mask = image.select(0).unitScale(0.000316, 3).multiply(255).toByte().connectedComponents(ee.Kernel.rectangle(1,1), 100);
-     return image.updateMask(mask.select(0)).set('system:time_start', image.get('system:time_start'));  
-   };
-   
-   /** Mask edges. This function requires that the input image has one VH or VV band, and an 'angle' bands.  */
-   exports.f_mask_edges = function(image) {
-     var output = maskAngGT30(image);
-     output = maskAngLT452(output);
-     output = maskEdge(output);
-     return output.set('system:time_start', image.get('system:time_start'));
-   };
-   
+/** (mask out angles >= 45.23993) */
+var maskAngLT452 = function(image) {
+ var ang = image.select(['angle']);
+ return image.updateMask(ang.lt(45.23993)).set('system:time_start', image.get('system:time_start'));
+};
+
+/** Function to mask out edges of images using angle.
+ * (mask out angles <= 30.63993) */
+var maskAngGT30 = function(image) {
+ var ang = image.select(['angle']);
+ return image.updateMask(ang.gt(30.63993)).set('system:time_start', image.get('system:time_start'));
+};
+
+/** Remove edges.
+ * Source: Andreas Vollrath */
+var maskEdge = function(image) {
+  var mask = image.select(0).unitScale(-25, 5).multiply(255).toByte().connectedComponents(ee.Kernel.rectangle(1,1), 100);
+  return image.updateMask(mask.select(0)).set('system:time_start', image.get('system:time_start'));  
+};
+
+/** Mask edges. This function requires that the input image has one VH or VV band, and an 'angle' bands.  */
+exports.f_mask_edges = function(image) {
+  var db_img = helper.lin_to_db(image)
+  var output = maskAngGT30(db_img);
+  output = maskAngLT452(output);
+  output = maskEdge(output);
+  output = helper.db_to_lin(output);
+  return output.set('system:time_start', image.get('system:time_start'));
+};
