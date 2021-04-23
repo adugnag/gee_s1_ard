66911e9650512280907e3ec4966d57452ddecd62
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2021-04-23 10:50:03 +0200

    Update s1_ard_test.js

--- javascript/s1_ard_test.js
+++ javascript/s1_ard_test.js
@@ -1,12 +1,12 @@
-/** File: s1_ard_test.js
+/* File: s1_ard_test.js
 Version: v1.2
 Date: 2021-03-10
-Authors: Mullissa A., Vollrath A.,  Reiche J., Slagter B., Balling J. , Gou Y., Braun, C.
+Authors: Mullissa A., Vollrath A., Gorelick N.,  Reiche J., Slagter B., Balling J. , Gou Y., Braun, C.
 Description: This script creates an analysis ready S1 image collection.
 
-    params:
-        geometry: The region to include imagery within. (ee.Geometry); The user has to interactively draw a bounding box within the map window or explicitly give coordinates in params.
-        ORBITS:  The orbits to include. (string: ASCENDING, DESCENDING OR BOTH)
+    Params:
+        geometry: The region to include imagery within. (ee.Geometry); The user has to interactively draw a bounding box within the map window.
+        ORBIT:  The orbits to include. (string:BOTH, ASCENDING or DESCENDING)
         START_DATE: The earliest date to include images for (inclusive).
         END_DATE: The latest date to include images for (exclusive).
         POLARIZATION: The Sentinel-1 image polarization to select for processing.
@@ -14,7 +14,7 @@ Description: This script creates an analysis ready S1 image collection.
             'VH' - selects the VH polarization.
             "VVVH' - selects both the VV and VH polarization for processing.
         APPLY_SPECKLE_FILTERING: true or false options to apply speckle filter
-        SPECKLE_FILTER: (Optional)    Type of speckle filter to apply.
+        SPECKLE_FILTER: (Optional)   Type of speckle filtering to apply. 
             'BOXCAR' - implements a boxcar filter on each individual image in the collection
             'LEE' - implements a Lee filter on each individual image in the collection based on (J.S. Lee et al. 1980)
             'GAMMA MAP' - implements a Gamma maximum a-posterior speckle filter on each individual image in the collection based on (Lopez et al. 1990
@@ -26,20 +26,16 @@ Description: This script creates an analysis ready S1 image collection.
                                   the filtering is applied to each image in the collection individually. Whereas, the 
                                   Multitemporal Speckle filter based on the approach by Quegan and Yu 2001 with any of the above mentioned speckle filters.
         SPECKLE_FILTER_KERNEL_SIZE: is the size of the filter spatial window applied in speckle filtering. must be a positive integer.
-        NR_OF_IMAGES: is the number of multi-temporal images to use in the multi-temporal filter framework.
+        NR_OF_IMAGES: is the number of images to use in the multi-temporal filter framework.
         APPLY_BORDER_NOISE_CORRECTION: (Optional) true or false options to apply additional Border noise correction:
-        TERRAIN_FLATTENING - (Optional) true or false option to apply Terrain correction based on Vollrath et al. 2020 and Hoekman and Reiche 2015. 
-        TERRAIN_FLATTENING_MODEL - which model to use for radiometric terrain flattening (DIRECT, or VOLUME)
-        DEM - digital elevation model (DEM) to use (as EE asset)
-        TERRAIN_FLATTENING_ADDITIONAL_LAYOVER_SHADOW_BUFFER - buffer parameter for passive layover/shadow mask in meters
-        FORMAT - the output format for the processed collection. this can be 'LINEAR' or 'DB'.
-        CLIP_TO_ROI - (Optional) Resizes the processed images to the region of interest.
-        SAVE_ASSETS - (Optional) Exports the processed collection to an EE asset.
+        TERRAIN_FLATTENING : (Optional) true or false option to apply Terrain correction based on Vollrath et al. 2020 and Hoekman and Reiche 2015 is applied. 
+        TERRAIN_FLATTENING_MODEL : which model to use for radiometric terrain flattening (DIRECT, or VOLUME)
+        DEM : which digital elevation model (DEM) to use (as ee asset)
+        TERRAIN_FLATTENING_ADDITIONAL_LAYOVER_SHADOW_BUFFER : buffer parameter for layover/shadow mask in meters
+        FORMAT : the output format for the processed collection. this can be 'LINEAR' or 'DB'.
+        SAVE_ASSETS : (Optional) Exports the processed collection to an asset.
     Returns:
-        An ee.ImageCollection with an analysis ready Sentinel 1 imagery with the following bands:
-            VV;
-            VH;
-            angle.
+        An ee.ImageCollection with an analysis ready Sentinel 1 imagery with the specified polarization images and angle band.
 **/
 
 var wrapper = require('users/adugnagirma/s1_ard_master:wrapper');
@@ -52,43 +48,37 @@ var helper = require('users/adugnagirma/s1_ard_master:utilities');
 
 var params = {//1. Data Selection
               START_DATE: "2019-01-01",
-              STOP_DATE: "2019-01-02",
+              STOP_DATE: "2019-05-30",
               POLARIZATION:'VVVH',
-              ORBIT : 'BOTH',
+              ORBIT : 'DESCENDING',
               DEM: ee.Image('USGS/SRTMGL1_003'),
-              geometry: ee.Geometry.Polygon([[[104.80, 11.61],[104.80, 11.36],[105.16, 11.36],[105.16, 11.61]]], null, false),
+              geometry: ee.Geometry.Polygon([[[112.05, -0.25],[112.05, -0.45],[112.25, -0.45],[112.25, -0.25]]], null, false),
               //2. Additional Border noise correction
-              APPLY_BORDER_NOISE_CORRECTION: false,
+              APPLY_BORDER_NOISE_CORRECTION: true,
               //3.Speckle filter
               APPLY_SPECKLE_FILTERING: true,
               SPECKLE_FILTER_FRAMEWORK: 'MULTI',
-              SPECKLE_FILTER: 'LEE SIGMA',
+              SPECKLE_FILTER: 'GAMMA MAP',
               SPECKLE_FILTER_KERNEL_SIZE: 9,
               NR_OF_IMAGES: 10,
               //4. Radiometric terrain normalization
               APPLY_TERRAIN_FLATTENING: true,
               TERRAIN_FLATTENING_MODEL: 'VOLUME',
-              TERRAIN_FLATTENING_ADDITIONAL_LAYOVER_SHADOW_BUFFER: 0,
+              TERRAIN_FLATTENING_ADDITIONAL_LAYOVER_SHADOW_BUFFER: 20,
               //5. Output
               FORMAT : 'DB',
-              CLIP_TO_ROI: false,
               SAVE_ASSETS: false
 }
 
 //---------------------------------------------------------------------------//
 // DO THE JOB
 //---------------------------------------------------------------------------//
-// Select S1 GRD ImageCollection
-var s1 = ee.ImageCollection('COPERNICUS/S1_GRD_FLOAT')
-      .filter(ee.Filter.eq('instrumentMode', 'IW'))
-      .filter(ee.Filter.eq('resolution_meters', 10))
-      .filterDate(params.START_DATE, params.STOP_DATE)
-      .filterBounds(params.geometry);
 
 //Preprocess the S1 collection
-var s1_preprocces = wrapper.s1_preproc(s1, params);
+var s1_preprocces = wrapper.s1_preproc(params);
 
-
+var s1 = s1_preprocces[0]
+s1_preprocces = s1_preprocces[1]
 
 //---------------------------------------------------------------------------//
 // VISUALIZE
@@ -105,7 +95,7 @@ if (params.POLARIZATION=='VVVH'){
     else {
     var s1_preprocces_view = s1_preprocces.map(helper.add_ratio_lin);
     var s1_view = s1.map(helper.add_ratio_lin);
-    visparam = {bands:['VV','VH','VVVH_ratio'], min: [0, 0, 0],max: [0.5, 0.5, 20]}
+    visparam = {bands:['VV','VH','VVVH_ratio'], min: [0.01, 0.0032, 1.25],max: [1, 0.31, 31.62]}
     }
 }
 else {
@@ -121,10 +111,13 @@ else {
     }
 }
 
-Map.centerObject(params.geometry, 13);
+
+
+Map.centerObject(params.geometry, 12);
 
 Map.addLayer(s1_view.first(), visparam, 'First image in the input S1 collection', true);
 Map.addLayer(s1_preprocces_view.first(), visparam, 'First image in the processed S1 collection', true);
+
 
 //---------------------------------------------------------------------------//
 // EXPORT
