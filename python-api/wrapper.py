69ad1e645eefcbbe68a27cd2c3fe339a6fc733e8
Adugna Mullissa <48068921+adugnag@users.noreply.github.com>
2024-10-09 15:53:42 -0700

    Update wrapper.py

--- python-api/wrapper.py
+++ python-api/wrapper.py
@@ -133,10 +133,13 @@ def s1_preproc(params):
     s1 = ee.ImageCollection('COPERNICUS/S1_GRD_FLOAT')\
         .filter(ee.Filter.eq('instrumentMode', 'IW'))\
         .filter(ee.Filter.eq('resolution_meters', 10)) \
-        .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))\
         .filterDate(START_DATE, STOP_DATE) \
         .filterBounds(ROI)
 
+    if POLARIZATION == 'VV':
+        s1 = s1.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+    else:
+        s1 = s1.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
 
     # select orbit
     if (ORBIT != 'BOTH'):
